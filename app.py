8ed7b3375f8c404754c0efade156c57257fd39d8
ym1ng <zymbest0424@gmail.com>
2014-10-18 16:16:45 +0800

    add BaseHandler

--- app.py
+++ app.py
@@ -3,14 +3,32 @@ import tornado.ioloop
 import tornado.web
 
 
-class MainHandler(tornado.web.RequestHandler):
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie('user')
+
+class MainHandler(BaseHandler):
+    @tornado.web.authenticated
     def get(self):
         self.write('test')
 
 
+class LoginHandler(BaseHandler):
+    def get(self):
+        self.write('login')
+
+    def post(self):
+        self.set_secure_cookie('user',self.get_argument('name'))
+
+settings = {
+    'cookie_secret': '123456789',
+    'login_url': '/login'
+
+}
 application = tornado.web.Application([
     (r'/', MainHandler),
-])
+    (r'/login', LoginHandler),
+],**settings)
 
 
 if __name__ == '__main__':
